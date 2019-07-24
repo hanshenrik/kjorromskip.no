6479d0f1fca0c19397ef98a24305554821aaf0c4
hanshenrik <hh.gronsleth@gmail.com>
2019-07-25 00:17:46 +0200

    Add some basic placeholder stuff

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,50 @@
 </script>
 
 <style>
-  h1 {
-    color: purple;
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: 10rem;
+  }
+
+  .floating-spaceship {
+    animation-name: move-around;
+    animation-delay: 1s;
+    animation-duration: 10s;
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes move-around {
+    0% {
+      transform: translate(0, 0) rotate(0);
+    }
+    10% {
+      transform: translate(100px, -100px) rotate(0);
+    }
+    25% {
+      transform: translate(80px, 0) rotate(90deg);
+    }
+    50% {
+      transform: translate(150px, 100px) rotate(30deg);
+    }
+    55% {
+      transform: translate(200px, 100px) rotate(70deg);
+    }
+    75% {
+      transform: translate(50px, 300px) rotate(-170deg);
+    }
+    90% {
+      transform: translate(0, 50px) rotate(270deg);
+    }
+    100% {
+      transform: translate(0, 0) rotate(-360deg);
+    }
   }
 </style>
 
-<h1>{name}!</h1>
+<div class="wrapper">
+  <div class="floating-spaceship">{name}</div>
+</div>
