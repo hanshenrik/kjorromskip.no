e750f103504a9e60658ab05567f05881faca0215
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-04-04 15:40:37 +0200

    Add Songkick gig section

--- src/App.svelte
+++ src/App.svelte
@@ -119,6 +119,30 @@
       >DM på Instagram</a
     >, så finner vi ut av det!
   </div>
+  <div>
+    <h1>Konserter</h1>
+    <a
+      href="https://www.songkick.com/artists/10064686"
+      class="songkick-widget"
+      data-theme="dark"
+      data-track-button="on"
+      data-detect-style="off"
+      data-background-color="rgb(0,0,0,1)"
+      data-font-color="rgb(255,255,255,1)"
+      data-button-bg-color="rgb(255,255,255,1)"
+      data-button-text-color="rgb(0,0,0,1)"
+      data-locale="en"
+      data-other-artists="on"
+      data-share-button="on"
+      data-country-filter="on"
+      data-rsvp="on"
+      data-request-show="on"
+      data-past-events="off"
+      data-past-events-offtour="off"
+      data-remind-me="off"
+      style="display: none;">Songkick</a
+    >
+  </div>
   <div class="contact">
     <h1>Kontakt</h1>
     <a href="mailto:kjor.romskip@gmail.com">kjor.romskip@gmail.com</a>
@@ -183,7 +207,9 @@
   }
 
   .cover-link .cover {
-    transition: filter 1s ease-in-out, transform 0.3s ease-in-out,
+    transition:
+      filter 1s ease-in-out,
+      transform 0.3s ease-in-out,
       box-shadow 0.3s ease-in-out;
   }
   .cover-link:hover .cover {
